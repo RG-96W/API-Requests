7c2999788d3da84ed6b89904ae0a49f0ea26f59a
Rafael Guilherme <115187383+RG-96W@users.noreply.github.com>
2023-10-06 18:22:57 -0300

    atualização no models

--- src/models/request.js
+++ src/models/request.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 
 const comentarioSchema = new mongoose.Schema({
@@ -11,6 +10,10 @@ const comentarioSchema = new mongoose.Schema({
 });
 
 const chamadoSchema = new mongoose.Schema({
+  id_interno: {
+    type: Number,
+    unique: true,
+  },
   titulo: {
     type: String,
     required: true,
@@ -42,6 +45,27 @@ const chamadoSchema = new mongoose.Schema({
   cliente: String,
   data_previsao: Date,
   comentarios: [comentarioSchema],
+});
+
+// Pré-salvar o documento para definir o ID interno automaticamente
+chamadoSchema.pre('save', async function (next) {
+  if (!this.id_interno) {
+    // Encontre o último chamado e obtenha seu ID interno
+    const ultimoChamado = await this.constructor.findOne({}, {}, { sort: { id_interno: -1 } });
+    const novoIDInterno = ultimoChamado ? ultimoChamado.id_interno + 1 : 1;
+    this.id_interno = novoIDInterno;
+  }
+
+  // Calcule a data de vencimento com base na prioridade
+  const prioridadeDias = {
+    Baixa: 2, // 48 horas
+    Média: 1, // 24 horas
+    Alta: 0.5, // 12 horas
+  };
+  const diasParaVencimento = prioridadeDias[this.prioridade] || 1; // Padrão para Média
+  this.data_previsao = new Date(Date.now() + diasParaVencimento * 24 * 60 * 60 * 1000);
+
+  next();
 });
 
 const requests = mongoose.model('Requests', chamadoSchema);
